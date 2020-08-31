ca7b48752f316fb6443f2ecdddedd2cafd62a903
Emanuele Correa <manuncorrea@hotmail.com>
2020-08-31 17:00:02 -0300

    working with DTO data

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,11 +1,16 @@
 import { uuid } from 'uuidv4';
 
+interface ApopointmentConstructor {
+    provider: string;
+    date: Date;
+}
+
 class Apopointment {
     id: string;
     provider: string;
     date: Date;
 
-    constructor(provider: string, date: Date){
+    constructor({provider, date}: Omit<Apopointment, 'id'>){
         this.id = uuid();
         this.provider = provider;
         this.date = date;
